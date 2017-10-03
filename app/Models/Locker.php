fcd8e05f8cce7d23b4535fc51e95bd04d5c6c97b
Titipat <ttpsvb@gmail.com>
2017-10-03 17:08:26 +0700

    Add blank models, enable facade, eloquent

---
+++ app/Models/Locker.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Locker extends Model
+{
+    function owner()
+    {
+        // TODO implement this
+        // return $this->;
+    }
+}
